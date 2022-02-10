36c9213e9132d75737e4e44dc671f39e65cbca14
QQmental <39618121+QQmental@users.noreply.github.com>
2022-02-10 12:38:36 +0800

    Update AVL_tree_algo.h

--- AVL_tree_algo.h
+++ AVL_tree_algo.h
@@ -1,35 +1,46 @@
-#ifndef AVL_TREE_H_INCLUDED
-#define AVL_TREE_H_INCLUDED
-#include <stdio.h>
-#include <assert.h>
-#define AVL_Tree(tree, k_size, d_size)\
-    struct AVL_Tree *tree ;\
-    AVL_Tree_init(tree, k_size, d_size);
-
+#ifndef AVL_TREE_ALGO_H_INCLUDED
+#define AVL_TREE_ALGO_H_INCLUDED
+#include"AVL_tree.h"
+#define _SELF ((_AVL_Tree*)self)
+#define COPYDATA(node, src)\
+    memcpy((void*)(((char*)node)+src##_offset),src, _SELF-> src##_size);
+struct AVL_node
+{
+    struct AVL_node *chd[2];
+    int height;
+};
+/*
 struct AVL_Tree
 {
     int (*Insert)(struct AVL_Tree *self, void *key, void *data);
     int (*Delete)(struct AVL_Tree *self, void *key);
     int (*IsFound)(struct AVL_Tree *self, void *key);
     int (*KeyComp)(void *key1, void *key2);
-    void (*SetKeyComp)(struct AVL_Tree *self, int(*)(void*, void*));
-    void (*PrintNode)(void *, void *);
-    void (*Traverse)(struct AVL_Tree *self);
+    void (*SetKeyComp)(int(*)(void*, void*));
+    void (*PrintNodes)(struct AVL_Tree *);
     void *FoundNodeData;
-    void (*KeyDestroy)(void *key);
-    void (*DataDestroy)(void *data);
-
-    void (*DefaultKeyDtor)(void *key);
-    void (*DefaultDataDtor)(void *data);
-
-    void (*SetKeyDestroy)(struct AVL_Tree *self, void(*)(void*));
-    void (*SetDataDestroy)(struct AVL_Tree *self, void(*)(void*));
-
-    void (*clean)(struct AVL_Tree *self);
 };
+*/
+struct _AVL_Tree
+{
+    struct AVL_Tree tree;
+    size_t key_size, data_size;
+    struct AVL_node *root;
+    int finished ;
+    void *private_cache;
+};
+typedef struct AVL_Tree AVL_Tree;
+typedef struct AVL_node AVL_node;
+typedef struct _AVL_Tree _AVL_Tree;
 
-struct AVL_Tree* AVL_Tree_init(size_t key_size, size_t data_size);
+
+int AVL_IsFound_algo(AVL_Tree *self, AVL_node *node ,void *key);
+
+AVL_node *AVL_Insert_algo(AVL_Tree *self, AVL_node *node ,void *key);
+
+void AVL_traverse_algo(AVL_Tree *self, AVL_node *node);
 
 
 
-#endif // AVL_TREE_H_INCLUDED
+AVL_node *AVL_Delete_algo(AVL_Tree *self, AVL_node *node, void *key);
+#endif // AVL_TREE_ALGO_H_INCLUDED
